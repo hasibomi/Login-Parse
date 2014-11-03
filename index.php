7743eb4747878cd3e583b51e68837d48ec5023d1
Hasibur Rahman Omi <hrahmanomi@gmail.com>
2014-11-04 05:03:51 +0600

    Design the login page

--- index.php
+++ index.php
@@ -1,15 +1,18 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta name="viewport" content="width=device-with," initial-scale="1">
+	<title>Login using PARSE</title>
+	<link rel="stylesheet" href="assets/css/foundation.min.css">
+	<link rel="stylesheet" href="css/normalize.css">
+	<script src="assets/js/vendor/modernizr.js"></script>
+</head>
+<body>
 
-require 'vendor/autoload.php';
- 
-use Parse\ParseClient;
- 
-ParseClient::initialize('H1AkucSxb1wAgm2MVeRRyICguegVakt5XicYzvdc', 'RlmmRI3YbTqEo3SJb2RBQEkrj3dvHypX63T8y4oL', '1iGm2dOwll70pc1rLQVbUOc5IiTEgz6LqfQZW97o');
-
-use Parse\ParseObject;
- 
-$testObject = ParseObject::create("TestObject");
-$testObject->set("foo", "bar");
-$testObject->save();
-
-?>
+	<script src="assets/js/vendor/jquery.js"></script>
+	<script src="assets/js/foundation.min.js"></script>
+	<script>
+	$(document).foundation();
+	</script>
+</body>
+</html>
